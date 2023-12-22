463fbc654a4aad4ce3748c65deba99eae1a3eb57
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-22 20:20:46 +0100

    added rooms

--- src/main/java/Exercise.java
+++ src/main/java/Exercise.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise {
     public static void main(String[] args) {
-        CinemaHall cinemaHall = new CinemaHall("Nazwa", RoomDimensionType.type_2D);
+        List<CinemaHall>Cinema = new ArrayList<>();
+        Cinema.add(new CinemaHall("Room 1", RoomDimensionType.type_2D));
+        Cinema.add(new CinemaHall("Room 2", RoomDimensionType.type_2D));
+        Cinema.add(new CinemaHall("Room 3", RoomDimensionType.type_2D));
+        Cinema.add(new CinemaHall("Room 4", RoomDimensionType.type_2D));
+        Cinema.add(new CinemaHall("Room 5", RoomDimensionType.type_3D));
+        Cinema.add(new CinemaHall("Room 6", RoomDimensionType.type_3D));
+        Cinema.add(new CinemaHall("Room 7", RoomDimensionType.type_3D));
+        Cinema.add(new CinemaHall("Room 8", RoomDimensionType.type_3D));
+        Cinema.add(new CinemaHall("Room 9", RoomDimensionType.type_4D));
+        Cinema.add(new CinemaHall("Room 10", RoomDimensionType.type_4D));
+
     }
+
 }
